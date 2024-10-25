e1def822d76d6c954deb7cd7499cb82a3d3e54a5
Ch1n-ch1nless <aleksandrov.oa@phystech.edu>
2024-10-25 15:06:50 +0300

    Implement class Canvas

--- source/View/canvas.cpp
+++ source/View/canvas.cpp
@@ -36,14 +36,191 @@ void Layer::setPixel(sfm::vec2i pos, sfm::Color pixel)
 
 void Layer::resize(vec2i new_size)
 {
-    size_ = new_size;
-    pixels_.resize(static_cast<size_t>(new_size.x * new_size.y));
+    std::vector<sfm::Color> new_pixels(static_cast<size_t>(new_size.x * new_size.y));
+
+    int width  = std::min(size_.x, new_size.x);
+    int height = std::min(size_.y, new_size.y);
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            new_pixels.at(y * new_size.x + x) = pixels_.at(y * size_.x + x);
+        }
+    }
+
+    size_   = new_size;
+    pixels_ = std::move(new_pixels);
 }
 
 /*============================================================================*/
 
 /*=========================< Canvas implementation >==========================*/
 
+Canvas::Canvas(vec2i size) :
+    temp_layer_(std::make_unique<Layer>(size)),
+    layers_(1),
+    size_(size),
+    texture_(),
+    sprite_(),
+    last_mouse_pos_({0, 0}),
+    is_pressed_(false) 
+{
+    texture_.create(static_cast<unsigned int>(size_.x),
+                    static_cast<unsigned int>(size_.y));
 
+    layers_.push_back(std::make_unique<Layer>(size_));
+}
+
+void Canvas::draw(psapi::IRenderWindow* renderWindow)
+{
+    for (const auto& layer : layers_)
+    {
+        drawLayer(layer.get(), renderWindow);
+    }
+
+    drawLayer(temp_layer_.get(), renderWindow);
+
+
+}
+
+bool Canvas::update(const IRenderWindow* renderWindow, const sfm::Event& event)
+{
+    if (event.type == Event::EventType::MouseButtonPressed)
+    {
+        is_pressed_ = true;
+    }
+    else if (event.type == Event::EventType::MouseButtonReleased)
+    {
+        is_pressed_ = false;
+    }
+
+    if (event.type == Event::EventType::MouseMoved)
+    {
+        last_mouse_pos_.x = event.mouseButton.x;
+        last_mouse_pos_.y = event.mouseButton.y;
+    }
+}
+
+ILayer* Canvas::getLayer(size_t index)
+{
+    return layers_.at(index).get();
+}
+
+const ILayer* Canvas::getLayer(size_t index) const
+{
+    return layers_.at(index).get();
+}
+
+ILayer* Canvas::getTempLayer()
+{
+    return temp_layer_.get();
+}
+
+const ILayer* Canvas::getTempLayer() const
+{
+    return temp_layer_.get();
+}
+
+void Canvas::cleanTempLayer()
+{
+    sfm::Color pixel = {0, 0, 0, 0};
+
+    for (int y = 0; y < size_.y; y++)
+    {
+        for (int x = 0; x < size_.x; x++)
+        {
+            temp_layer_->setPixel({x, y}, pixel);
+        }
+    }
+}
+
+size_t Canvas::getNumLayers() const
+{
+    return layers_.size();
+}
+
+size_t Canvas::getActiveLayerIndex() const
+{
+    return active_layer_id_;
+}
+
+void Canvas::setActiveLayerIndex(size_t index)
+{
+    assert(index < layers_.size());
+    active_layer_id_ = index;
+}
+
+bool Canvas::insertLayer(size_t index, std::unique_ptr<ILayer> layer)
+{
+    assert(index <= layers_.size());
+
+    std::unique_ptr<Layer> new_layer = std::make_unique<Layer>(size_);
+
+    for (int x = 0; x < size_.x; x++) {
+        for (int y = 0; y < size_.y; y++) {
+            new_layer->setPixel({x, y}, layer->getPixel({x, y}));
+        }
+    }
+
+    layers_.insert(layers_.begin() + (long)index, std::move(new_layer));
+    return true;
+}
+
+bool Canvas::removeLayer(size_t index)
+{
+    assert(index < layers_.size());
+
+    layers_.erase(layers_.begin() + (long)index);
+    return true;
+}
+
+bool Canvas::insertEmptyLayer(size_t index)
+{
+    assert(index <= layers_.size());
+
+    std::unique_ptr<Layer> new_layer = std::make_unique<Layer>(size_);
+
+    layers_.insert(layers_.begin() + (long)index, std::move(new_layer));
+    return true;
+}
+
+void Canvas::setPos(sfm::vec2i pos)
+{
+    pos_ = pos;
+}
+
+void Canvas::setSize(sfm::vec2i size)
+{
+    size_ = size;
+
+    temp_layer_->resize(size);
+    for (auto& layer : layers_) {
+        layer->resize(size);
+    }
+}
+
+void Canvas::setScale(sfm::vec2f scale)
+{
+    scale_ = scale;
+}
+
+sfm::vec2i Canvas::getMousePosition() const
+{
+    return last_mouse_pos_;
+}
+
+bool Canvas::isPressed() const
+{
+    return is_pressed_;
+}
+
+void Canvas::drawLayer(const Layer* layer, IRenderWindow* renderWindow)
+{
+    texture_.update(layer->pixels_.data());
+    sprite_.setTexture(&texture_);
+    sprite_.setScale(scale_.x, scale_.y);
+    renderWindow->draw(&sprite_);
+}
 
 /*============================================================================*/
--- source/View/canvas.hpp
+++ source/View/canvas.hpp
@@ -28,10 +28,54 @@ namespace psapi
     class Canvas : public ICanvas
     {
     public:
-        //...
+         Canvas(vec2i init_size);
+        ~Canvas() = default;
+
+        virtual void draw(psapi::IRenderWindow* renderWindow)         override;
+        virtual bool update(const psapi::IRenderWindow* renderWindow,
+                            const psapi::sfm::Event& event)           override;
+
+        virtual ILayer*       getLayer(size_t index)       override;
+        virtual const ILayer* getLayer(size_t index) const override;
+
+        virtual ILayer*       getTempLayer()       override;
+        virtual const ILayer* getTempLayer() const override;
+
+        virtual void cleanTempLayer() override;
+
+        virtual size_t getNumLayers() const override;
+
+        virtual size_t getActiveLayerIndex() const override;
+        virtual void   setActiveLayerIndex(size_t index) override;
+
+        virtual bool insertLayer     (size_t index, std::unique_ptr<ILayer> layer) override;
+        virtual bool removeLayer     (size_t index) override;
+        virtual bool insertEmptyLayer(size_t index) override;
+
+        virtual void setPos  (sfm::vec2i pos)   override;
+        virtual void setSize (sfm::vec2i size)  override;
+        virtual void setScale(sfm::vec2f scale) override;
+
+        virtual sfm::vec2i getMousePosition() const override;
+        virtual bool isPressed()         const override;
 
     private:
-        //...
+        void drawLayer(const Layer* layer, IRenderWindow* renderWindow);
+
+        std::vector<std::unique_ptr<Layer>> layers_;
+
+        std::unique_ptr<Layer> temp_layer_;
+
+        vec2i size_;
+        vec2i pos_;
+        vec2f scale_;
+
+        size_t active_layer_id_;
+
+        psapi::sfm::Texture texture_;
+        psapi::sfm::Sprite sprite_;
+        vec2i last_mouse_pos_;
+        bool is_pressed_ = false;
     };
 
 }
